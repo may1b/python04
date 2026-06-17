0776b65e0be2752774e7f8c84cccd40c87588816
May <magrass@student.42heilbronn.de>
2026-06-17 07:56:37 +0200

    done ex0!

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main() -> None:
+    if len(sys.argv) <= 1:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    print("=== Cyber Archives Recovery ===")
+    fname = sys.argv[1]
+    print(f"Accessing file '{fname}'")
+    try:
+        file = open(fname, "r")
+        print("---\n")
+        print(file.read())
+        print("---")
+        file.close()
+        print(f"File {fname} closed.")
+    except Exception as err:
+        print(f"Error opening file '{fname}': {err}")
+        return
+
+
+if __name__ == "__main__":
+    main()
